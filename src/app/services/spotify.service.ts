964cd350afc24681efd4b64af208a276a9396a13
paulQR <paulqr10@gmail.com>
2019-07-19 08:41:15 +0200

    1 file modified

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 
 /*
+ forma automatica de importar servicio, no es necesario importarlo en el fichero
+ "app.module.app"
+ */
 @Injectable({
   providedIn: 'root'
 })
-*/
-@Injectable()
+
+//@Injectable()
 export class SpotifyService{
     constructor(){
         console.log('Spotify service listo');
