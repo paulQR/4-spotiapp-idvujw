1e2639890ad37a369a16e217b38ae524f7d5af65
paulQR <paulqr10@gmail.com>
2019-07-19 10:06:14 +0200

    1 file modified

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,11 +1,12 @@
 import{Component} from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html', 
 })
 export class SearchComponent {
-    constructor(){}
+    constructor(private spotify: SpotifyService){}
 
     buscar(termino: string){
       console.log(termino);
